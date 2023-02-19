a3fff129ebf6a10726622b6ca85e5123f45f2652
Sreshtha <sreshtha.mehrotra@gmail.com>
2023-02-20 04:38:01 +0530

    offset and padding

--- app/src/main/java/com/example/mycomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/mycomposeapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mycomposeapp
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -9,22 +10,26 @@ import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           Row(
+           Column(
                modifier = Modifier
-                    .fillMaxSize(0.5f)
-                    .background(Color.Yellow),
-               verticalAlignment = Alignment.CenterVertically,
-               horizontalArrangement = Arrangement.Center
-                ) {
-                Text(text = "Hello there!")
-                Text(text = "Hello")
-                Text(text = "Hello")
-            }
+                   .background(Color.Cyan)
+                   .fillMaxHeight(.5f)
+                   .fillMaxWidth(0.5f)
+                   .padding(top = 40.dp)
+
+           ) {
+               Text(text = "Hello",
+                   modifier = Modifier
+                       .offset(0.dp,20.dp))
+               Spacer(modifier = Modifier.height(50.dp))
+               Text(text = "World")
+           }
         }
     }
 }
