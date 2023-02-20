ccc1037373b3a7b5bfe919a10893e9dd6d305fed
Sreshtha <sreshtha.mehrotra@gmail.com>
2023-02-20 10:32:04 +0530

    image card sample

--- app/src/main/java/com/example/mycomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/mycomposeapp/MainActivity.kt
@@ -4,35 +4,90 @@ import android.os.Bundle
 import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           Column(
-               modifier = Modifier
-                   .background(Color.Cyan)
-                   .fillMaxHeight(.5f)
-                   .fillMaxWidth(0.5f)
-                   .border(5.dp, Color.Magenta)
-                   .padding(5.dp)
-                   .border(5.dp, Color.Black)
-                   .padding(5.dp)
-                   .border(5.dp, Color.Red) // these are applied sequentially.
+            val painter = painterResource(id = R.drawable.batman)
+            val desc = "This is Batman"
+            val title = "I'm Batman!!"
 
-           ) {
-               Text(text = "Hello")
-               Spacer(modifier = Modifier.height(50.dp))
-               Text(text = "World")
-           }
+            Box(modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .padding(8.dp)) {
+                ImageCard(painter = painter, contentDesc =desc , title =title)
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDesc: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
+    ) {
+        Box(
+            modifier = Modifier // used for stacking instead of putting sequentially
+                .height(200.dp)
+        ) {
+
+            Image(
+                painter = painter,
+                contentDescription = contentDesc,
+                contentScale = ContentScale.Crop
+            )
+
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            Color.Black
+                        ),
+                        startY = 300f,
+
+                    )
+                ))
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomCenter
+            ) {
+                Text(text = title,
+                    style = TextStyle(color=Color.White, fontSize = 16.sp)
+                )
+            }
+
+
         }
     }
 }
